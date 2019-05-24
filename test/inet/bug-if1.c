1dfaf25d8c83b19d2e34b92d82f3eaaba4ef1062
Waldemar Brodkorb <wbx@openadk.org>
2019-05-24 11:52:30 +0200

    be more conformant, from Timur.R.Mustafin@mcst.ru

--- test/inet/bug-if1.c
+++ test/inet/bug-if1.c
@@ -16,6 +16,8 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
+#define _GNU_SOURCE 1
+
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
